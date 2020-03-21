ba768b8197483d7eecd1dbeeceb333c34363f937
munroe1786 <era3munroe@gmail.com>
2020-03-21 13:54:02 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solution here
+function findMatching(array, answer) {
+    return array.filter( name =>
+        name.toLowerCase() === answer.toLowerCase()
+    )
+}
+
+function fuzzyMatch(array, answer) {
+    return array.filter( name =>
+        name.toLowerCase().indexOf(answer.toLowerCase()) === 0
+    )
+}
+
+function matchName(array, answer) {
+    return array.filter( item => item.name === answer)
+}
